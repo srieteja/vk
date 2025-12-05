4a10534868ba298e103ffee893a0d0efd4edde2f
Srie Teja <srit1505@gmail.com>
2025-12-06 03:36:58 +0530

    fix: complete code

--- cmd/main.go
+++ cmd/main.go
@@ -87,4 +87,7 @@ func main() {
 	<-quit
 
 	log.Println("Shutting down server...")
-	ctx, cancel := context.WithTimeout(conte
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	server.Shutdown(ctx)
+}
